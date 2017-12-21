f83832fa0e0d6e4dfa4e4944a77f388031e6c34e
Eric Courville <edhsystems@gmail.com>
2017-12-21 08:15:44 +0000

    Did a crap ton to the ec2 script

--- New-EC2Instc.ps1
+++ New-EC2Instc.ps1
@@ -4,3 +4,64 @@
  We'll Tag the instance and the EBS Volume.
  We will create and configure the Security Groups
  #>
+
+#Create Instance Security Group
+$myVPC = Get-EC2Vpc
+$groupid = New-EC2SecurityGroup -VpcId $myVPC.VpcId -GroupName "LinuxWebServerSG" `
+    -GroupDescription "Apache Webserver base security group"
+    $ip1 = new-object Amazon.EC2.Model.IpPermission 
+    $ip1.IpProtocol = "tcp" 
+    $ip1.FromPort = 22 
+    $ip1.ToPort = 22
+    $ip1.IpRanges.Add("0.0.0.0/0") 
+
+    $ip2 = new-object Amazon.EC2.Model.IpPermission 
+    $ip2.IpProtocol = "tcp"
+    $ip2.FromPort = 80
+    $ip2.ToPort = 80
+    $ip2.IpRanges.Add("0.0.0.0/0")
+
+Grant-EC2SecurityGroupIngress -GroupId $groupid -IpPermissions @( $ip1, $ip2 )
+$updatedSG = $groupid | Get-EC2SecurityGroup
+$updatedSG
+
+# Create EBS Volume for WWW
+$volume = New-Object Amazon.EC2.Model.EbsBlockDevice
+$volume.VolumeSize = 20
+$volume.VolumeType = 'standard'
+$volume.DeleteOnTermination = $true
+
+$deviceMapping = New-Object Amazon.EC2.Model.BlockDeviceMapping
+$deviceMapping.DeviceName = '/dev/sda1'
+$deviceMapping.Ebs = $volume
+
+# Create a Tag object to apply to the reservation
+$myTags = @(
+    @{key="Name";value="PHP_Webserver"}, `
+    @{key="Environment";value="Development"}, `
+    @{key="Purpose";value="Webserver"}
+)
+
+$userData = @"
+!#/bin/bash
+
+apt-get -y install apache2 php libapache2-mod-php php-mcrypt php-mysql
+systemctl restart apache
+aws s3 cp s3://PowerShell-Demo/* /var/www/html
+"@
+
+$encodedUserData = [System.Text.Encoding]::UTF8.GetBytes($userData)
+$encodedData = [System.Convert]::ToBase64String($encodedUserData)
+# Create EC2 Instance
+$awsLinuxAmi = 'ami-aa2ea6d0'
+$myNewReservation = New-EC2Instance -ImageId $awsLinuxAmi -KeyName 'svcc2017' `
+                    -InstanceType 't2.micro' -MinCount 1 -MaxCount 1 `
+                    -SecurityGroupId $updatedSG.GroupId `
+                    -BlockDeviceMapping $deviceMapping `
+                    -UserData $encodedData
+                    -InstanceProfile_Name 'EC2Role'
+
+# Apply tags to new instance
+$myNewReservation.Instances.InstanceId|New-EC2Tag -Tag $myTags
+
+Start-EC2Instance -InstanceId $myNewReservation.Instances.InstanceId
--- www/index.php
+++ www/index.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="UTF-8">
-    <title><?php echo date('l jS \of F Y h:i:s A') ?></title>
+<title><?php echo date('l jS \of F Y h:i:s A') ?></title>
   </head>
   <body>
     <h1>
