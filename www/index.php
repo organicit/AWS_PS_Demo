916b802587ccc4006e2c85e7ff7325445c5ec5e5
Eric Courville <edhsystems@gmail.com>
2017-12-21 04:41:43 +0000

    added www directory and added html tags and a couple php functions

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title><?php echo date('l jS \of F Y h:i:s A') ?></title>
+  </head>
+  <body>
+    <h1>
+    <?php echo "Welcome to my Super PHP Site!"; ?>
+    </h1>
+  </body>
+</html>
+
