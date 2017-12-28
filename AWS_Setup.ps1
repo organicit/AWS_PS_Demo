4a2271cb7f60a0206b98585f32c3bdd9ce2d9c91
Eric Courville <edhsystems@gmail.com>
2017-12-28 20:24:11 +0000

    updated setup script

--- AWS_Setup.ps1
+++ AWS_Setup.ps1
@@ -1,1 +1,13 @@
 # Configure the AWS CLI
+
+# Option 1 Access Keys/Secret Keys
+# Example pinched from 
+# http://docs.aws.amazon.com/powershell/latest/userguide/specifying-your-aws-credentials.html
+Set-AWSCredential -AccessKey AKIAIOSFODNN7EXAMPLE `
+                  -SecretKey wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY `
+                  -StoreAs MyProfileName
+
+# Option 2 SAML Authentication
+#start-process  https://aws.amazon.com/blogs/security/enabling-federation-to-aws-using-windows-active-directory-adfs-and-saml-2-0/
+
+# Option 3 Role applied to EC2 Instances
