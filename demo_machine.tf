e4cffa610f7797980e625f3180f1028c4843eba5
Eric Courville <organicit-git@outlook.com>
2018-02-13 08:09:32 -0800

    bunch of stuff

---
+++ demo_machine.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_instance" "OsloDemo" {
+  ami                         = "ami-33fefd53"
+  instance_type               = "t2.medium"
+  subnet_id                   = "subnet-a2f23bc5"
+  associate_public_ip_address = true
+  security_groups             = ["sg-7bb83102"]
+  key_name                    = "aws_centos"
+  tags {
+        Name = "OsloDemo"
+  }
+}
---
+++ demo_setup.ps1
@@ -0,0 +1,5 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+
+choco install -y git.install
+choco install -y visualstudiocode
