925577b756ad14b0d94f1f21157fe267a1fba0ea
Eric Courville <organicit-git@outlook.com>
2018-02-13 17:50:44 +0000

    updated cloud formation template ps1 file

--- Creating-CFStacks.ps1
+++ Creating-CFStacks.ps1
@@ -2,7 +2,8 @@ $MyCFNStack = "organicit-test1"
 
 New-CFNStack -StackName $MyCFNStack `
              -TemplateURL https://s3.amazonaws.com/PowerShell-Demo/New_S3_Bucket.json `
-             -Parameter @{ ParameterKey="BucketName"; ParameterValue=$MyCFNStack }
+             -Parameter @( @{ ParameterKey="BucketName"; ParameterValue=$MyCFNStack }, `
+                        @{ ParameterKey="AccessCTL"; ParameterValue="private" })
 
 
 Update-CFNStack -StackName $MyCFNStack `
