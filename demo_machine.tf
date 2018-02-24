905d69fcb21f1ebc0781b67386bd92f418ba54f2
Eric Courville <organicit-git@outlook.com>
2018-02-23 18:46:17 -0800

    updated terraform

--- demo_machine.tf
+++ demo_machine.tf
@@ -9,6 +9,7 @@ resource "aws_instance" "OsloDemo" {
   associate_public_ip_address = true
   security_groups             = ["sg-7bb83102"]
   key_name                    = "aws_centos"
+  iam_instance_profile                 = "EC2Role"
   tags {
         Name = "OsloDemo"
   }
