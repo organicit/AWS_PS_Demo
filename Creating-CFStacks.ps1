f20d5f38caf00cc20bac4d7f88705c815499d26c
Eric Courville <organicit-git@outlook.com>
2018-03-14 18:25:57 -0700

    updated bucket name

--- Creating-CFStacks.ps1
+++ Creating-CFStacks.ps1
@@ -1,13 +1,13 @@
 $MyCFNStack = "organicit-test1"
 
 New-CFNStack -StackName $MyCFNStack `
-             -TemplateURL https://s3.amazonaws.com/PowerShell-Demo/New_S3_Bucket.json `
+             -TemplateURL https://s3.amazonaws.com/pdxugdemo/New_S3_Bucket.json `
              -Parameter @( @{ ParameterKey="BucketName"; ParameterValue=$MyCFNStack }, `
                         @{ ParameterKey="AccessCTL"; ParameterValue="PublicRead" })
 
 
 Update-CFNStack -StackName $MyCFNStack `
-             -TemplateURL https://s3.amazonaws.com/PowerShell-Demo/New_S3_Bucket.json `
+             -TemplateURL https://s3.amazonaws.com/pdxugdemo/New_S3_Bucket.json `
              -Parameter @( @{ ParameterKey="BucketName"; ParameterValue=$MyCFNStack }, `
                         @{ ParameterKey="AccessCTL"; ParameterValue="Private" })
 
